89d8e076ab77c5ef4795b4108eb7904c24359570
achanta3215 <achanta3215@gmail.com>
2017-07-30 09:20:44 +0530

    Sublime updated

--- read_json.ps1
+++
@@ -1,3 +0,0 @@
-param([string] $prop)
-$config = Get-Content config.json | ConvertFrom-Json
-$config.$prop
--- sublime.bat
+++ sublime.bat
@@ -1,3 +1,3 @@
-@echo off
-for /f "delims=" %%a in ('powershell.exe -ExecutionPolicy Bypass .\read_json.ps1 -prop "sublime"') do Set "$Path=%%a"
+rem @echo off
+for /f "delims=" %%a in ('powershell.exe -ExecutionPolicy Bypass %~dp0\readJson.ps1 -prop "sublime"') do Set "$Path=%%a"
 "%$Path%\subl.exe" %cd%
